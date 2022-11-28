b985bdc54d53b6da819bd1b647f780af9093c6fb
Marko Frei <108885340+cryptodev1990@users.noreply.github.com>
2022-11-28 17:15:14 +0100

    final

--- src/app/components/nft-collection/nft-collection.component.ts
+++ src/app/components/nft-collection/nft-collection.component.ts
@@ -55,7 +55,7 @@ export class NftCollectionComponent implements OnInit {
 
   async ngOnInit() {
 
-    const collectionAddr = "0x41f9b1bf4a506129eebc10981f2199863ddf442e3f526e18a64b403387c95cc5";
+    const collectionAddr = "0xaa1901608a3646851054936b873943d30d8c80789f8651ed1af2f4747316ef39";
 
     setInterval(() => {
       (async () => {
@@ -199,7 +199,7 @@ export class NftCollectionComponent implements OnInit {
     const wallet_type = localStorage.getItem('aptos-wallet-connector#last-connected-wallet-type');
 
     const moduleAddr = "0x55d3652e628e5cd7947232c4e896f558a88c6d2238f8f6e264fb7c1ada5aa2f9";
-    const collectionAddr = "0x41f9b1bf4a506129eebc10981f2199863ddf442e3f526e18a64b403387c95cc5";
+    const collectionAddr = "0xaa1901608a3646851054936b873943d30d8c80789f8651ed1af2f4747316ef39";
     const collectionName = "Beyond Owls";
 
     let sender;
@@ -212,7 +212,7 @@ export class NftCollectionComponent implements OnInit {
 
     const payload = {
       type: "entry_function_payload",
-      function: `${moduleAddr}::owl_nft3::mint_script`,
+      function: `${moduleAddr}::owl_nft6::mint_script`,
       arguments: [collectionAddr],
       type_arguments: [],
     };
